ff60809640912b0b8ce343162b0f0a914913aeb0
Stanislav Shurupin <stanislav.shurupin@gmail.com>
2019-06-30 21:48:49 +0300

    Added ability to listen to audio

--- Components/Card.js
+++ Components/Card.js
@@ -1,9 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { Audio } from 'expo';
+
+const source = {
+  uri: 'https://dictionary.cambridge.org/media/english-russian/uk_pron_ogg/u/ukh/ukhef/ukheft_029.ogg',
+};
 
 export default class Card extends React.Component {
     constructor(props) {
       super(props);
+    }
+
+    _playAndPause = () => {
+      this._playRecording();
+    }
+
+    async _playRecording() {
+      const { sound } = await Audio.Sound.create(
+        source,
+        {
+          shouldPlay: true,
+          isLooping: false,
+        },
+        null,
+      );
     }
   
     render() {
@@ -11,6 +31,11 @@ export default class Card extends React.Component {
         <View style={styles.card}>
           <Image style={styles.thumbnail} source={{uri: this.props.image}} />
           <Text style={styles.text}>This is card {this.props.name}</Text>
+          <TouchableOpacity style={styles.button} onPress={this._playAndPause}>
+            <Text style={styles.buttonText}>
+              Playing
+            </Text>
+          </TouchableOpacity>
         </View>
       )
     }
@@ -36,5 +61,17 @@ export default class Card extends React.Component {
       fontSize: 20,
       paddingTop: 10,
       paddingBottom: 10
+    },
+    button: {
+      width: 256,
+      height: 256 / 1.618,
+      margin: 5,
+      borderRadius: 10,
+      backgroundColor: '#fff',
+      justifyContent: 'center',
+    },
+    buttonText: {
+      textAlign: 'center',
+      backgroundColor: 'transparent',
     },
   })
