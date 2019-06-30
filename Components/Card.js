0ce3ee37711fb5790d80091ca9d4f9732c9a1059
Stanislav Shurupin <stanislav.shurupin@gmail.com>
2019-06-30 19:07:59 +0300

    Added SwipeCards

---
+++ Components/Card.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default class Card extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+  
+    render() {
+      return (
+        <View style={styles.card}>
+          <Image style={styles.thumbnail} source={{uri: this.props.image}} />
+          <Text style={styles.text}>This is card {this.props.name}</Text>
+        </View>
+      )
+    }
+  }
+
+  const styles = StyleSheet.create({
+    card: {
+      alignItems: 'center',
+      borderRadius: 5,
+      overflow: 'hidden',
+      borderColor: 'grey',
+      backgroundColor: 'white',
+      borderWidth: 1,
+      elevation: 1,
+    },
+    thumbnail: {
+      width: 300,
+      height: 300,
+    },
+    text: {
+      fontSize: 20,
+      paddingTop: 10,
+      paddingBottom: 10
+    },
+  })
---
+++ Components/NoMoreCards.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class NoMoreCards extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+  
+    render() {
+      return (
+        <View style={styles.noMoreCards}>
+          <Text>No more cards</Text>
+        </View>
+      )
+    }
+  }
+
+  const styles = StyleSheet.create({
+    noMoreCards: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  })
